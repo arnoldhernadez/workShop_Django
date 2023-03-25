d7b3113780fdcd744757f8bf788082a005a7c470
unknown <SENA@IBAAPRCLLFSP077.FormacionSena.RED>
2023-03-24 20:49:26 -0500

    marzo_24

--- appProductos/urls.py
+++ appProductos/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from . import views 
  
 urlpatterns = [ 
-    path('categorias/', views.verCategorias, name='categorias'),  ] 
+    path('categorias/', views.verCategorias, name='categorias'),  
+    path('productos/<str:idCategoria>', views.verProductosCategoria, name='productos'),
+    path('producto/<str:idProd>', views.verProducto, name='un_producto'),
+]
--- appProductos/views.py
+++ appProductos/views.py
@@ -14,3 +14,34 @@ def verCategorias(request):     #Consultar categorias
         'titulo': 'Categorias de Productos del Supermercado',     } 
     #renderizar 
     return render(request, 'productos/categorias.html', context) 
+
+
+def verProductosCategoria(request, idCategoria): #Consultar categorias
+    idCat = int(idCategoria)
+    nombreCat = Categoria.objects.get
+    listaProductos = Producto.objects.filter(categoria= idCat)
+
+
+#ensamblar context
+    context = {
+            'productos': listaProductos,
+            'titulo': 'Productos de la categoria ' + str(nombreCat),
+}
+#renderizar
+    return render(request, 'productos/productos.html', context)
+
+def verProducto(request, idProd, msj = None): #Consultar
+    idProd = int(idProd)
+    regProducto = Producto.objects.get(id= idProd) #ensamblar context
+    context = {
+        'producto': regProducto,
+        'titulo': 'Detalles de ' + str(regProducto.nombre),
+}
+
+
+    if msj:
+        context['mensaje']= msj #renderizar
+    return render(request, 'productos/producto.html', context)
+
+
+
